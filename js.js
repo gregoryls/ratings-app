d7450a22fe048276e104c781ac757f501ea43d57
Greg Schneider <greg53x@msn.com>
2023-01-26 00:01:35 -0700

    add an html button to display the list of books

--- js.js
+++ js.js
@@ -1,4 +1,6 @@
-let books = [];
+let books = '';
+let list = document.querySelector('#list ol');
+let listItem = document.createElement('li');
 Papa.parse('data/goodreads_library_export.csv', 
     {   
         header:true,
@@ -6,6 +8,29 @@ Papa.parse('data/goodreads_library_export.csv',
         complete: function(results){
             books = results.data;
             console.log(results);
+            console.log(books);
         }
     }
 )
+console.log(books);
+let btn = document.querySelector('#populate');
+btn.addEventListener('click',()=>{
+    for (let i=0;i<2;i++){
+        listItem.textContent = books[i].Title + ', ' + books[i].Author + 
+        ', ' + books[i]['My Rating'];
+        list.appendChild(listItem);
+    
+        console.log(listItem);
+    }
+    
+
+})
+
+for (let i=0;i<books.length;i++){
+    console.log(i);
+    listItem.textContent = books[i].Title + ', ' + books[i].Author + 
+    ', ' + books[i]['My Rating'];
+    list.appendChild(listItem);
+
+    console.log(listItem);
+}
